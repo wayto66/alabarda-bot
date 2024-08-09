c026c4ead6e38a57df88bb52d7d2b89082a470c0
wayTo <yurigarciadev@gmail.com>
2024-08-09 05:51:19 -0300

    https

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import fs from 'fs';
 import { AppModule } from './server/modules/main/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = fs.existsSync(
+    '../../../etc/letsencrypt/live/alabarda.link/',
+  )
+    ? {
+        key: fs.readFileSync(
+          '../../../etc/letsencrypt/live/alabarda.link/privkey.pem',
+        ),
+        cert: fs.readFileSync(
+          '../../../etc/letsencrypt/live/alabarda.link/fullchain.pem',
+        ),
+      }
+    : undefined;
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors({
     origin: '*',
     methods: 'GET,PUT,PATCH,POST,DELETE,OPTIONS',
   });
+
   await app.listen(3000);
 }
 bootstrap();
